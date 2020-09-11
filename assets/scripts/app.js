306f52360a9ece3727e118090297a760dbab36b3
Arshak Asriyan <aasriyan01@gmail.com>
2020-09-11 10:53:00 -0400

    added event listener for the add button click

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+function add(num1, num2) {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, '');
+}
 
-let calculationDescription = '(' + currentResult + ' + 10) * 3 / 2 - 1';
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add);
